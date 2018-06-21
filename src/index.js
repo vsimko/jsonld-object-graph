95fa9720579359a9da9abc05838e100d4b51fadd
Viliam Simko <viliam.simko@gmail.com>
2018-06-21 08:06:18 +0200

    refactor: use more ramda: type, compose - `type` from ramda  instead of custom `betterTypeOf` function -  the mapping `type2fn` is now smaller due to `compose`

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
-const { map, identity, composeP, prop, uniq, compose, isEmpty } = require('ramda')
+const { map, identity, prop, uniq, compose, composeP, isEmpty, type } = require('ramda')
 const jsonld = require('jsonld')
-
-/** Same as built-in javascript `typeof` but returns `array` for Arrays */
-function betterTypeOf (x) {
-  if (typeof x === 'object' && x instanceof Array) return 'array'
-  return typeof x
-}
 
 const compactWithContexts = contexts => async json => jsonld.compact(json, contexts)
 
@@ -23,28 +17,25 @@ async function jsonld2obj (json, contexts = {}) {
   // TODO: nicer would be to pass `id2obj` as parameter during the recursion
   const id2obj = {} // our temporary cache for objects with @id
   const type2fn = {} // map of functions handling different types during recursion
-  const dispatch = x => type2fn[betterTypeOf(x)](x)
+  const dispatch = x => type2fn[type(x)](x)
+  const resolveMappedObj = mapped => {
+    const id = mapped['@id'] // try to resolve objects if @id is present
+    if (id) {
+      if (!id2obj[id]) id2obj[id] = {} // new object found
+      return Object.assign(id2obj[id], mapped) // update cache
+    }
+    return mapped
+  }
 
   // now we can fill the map because our functions are recursive
   // and they refer to the `dispatch` function
   Object.assign(type2fn, {
-    string: identity,
-    number: identity,
-    boolean: identity,
-    undefined: identity,
-    array: compose(uniq, map(dispatch)),
-    object: x => {
-      // first recursively map the given json-tree
-      // json is always without cycles
-      const mapped = map(dispatch, x)
-
-      const id = x['@id'] // try to resolve objects if @id is present
-      if (id) {
-        if (!id2obj[id]) id2obj[id] = {} // new object found
-        return Object.assign(id2obj[id], mapped) // update cache
-      }
-      return mapped
-    }
+    String: identity,
+    Number: identity,
+    Boolean: identity,
+    Null: identity,
+    Array: compose(uniq, map(dispatch)),
+    Object: compose(resolveMappedObj, map(dispatch))
   })
 
   const processJsonRecursively =
