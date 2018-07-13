3c11bcf6adea692f30975cf916ee711539dbd19d
Viliam Simko <viliam.simko@gmail.com>
2018-07-13 12:07:53 +0200

    test: refactor parts to common.js and add new test

--- test/jsonld2obj.test.js
+++ test/jsonld2obj.test.js
@@ -1,28 +1,6 @@
 const { jsonld2obj } = require('../src')
 
-const jsonld = require('jsonld')
-
-const CONTEXTS = {
-  'http://test/ctx/person.jsonld': {
-    '@context': {
-      'name': 'http://xmlns.com/foaf/0.1/name',
-      'knows': {
-        '@id': 'http://xmlns.com/foaf/0.1/knows',
-        '@type': '@id'
-      }
-    }
-  }
-}
-
-jsonld.documentLoader = (url, callback) => {
-  if (url in CONTEXTS) {
-    return callback(null, {
-      contextUrl: null, // this is for a context via a link header
-      document: CONTEXTS[url], // this is the actual document that was loaded
-      documentUrl: url // this is the actual context URL after redirects
-    })
-  }
-}
+const { HALFLIFE_PERSON_NS, FOAF_NS, HALFLIFE_JSONLD } = require('./common')
 
 describe('#jsonld2obj', () => {
   it('should work for simple types', async () => {
@@ -41,39 +19,18 @@ describe('#jsonld2obj', () => {
   })
 
   it('should work with `@context`', async () => {
-    const myInstancesNs = 'http://test/person/'
-    const mySchemaUrl = 'http://test/ctx/person.jsonld'
-
-    const idGordon = myInstancesNs + 'Gordon'
-    const idAlyx = myInstancesNs + 'Alyx'
-
-    const data = [
-      {
-        '@context': mySchemaUrl,
-        '@id': idGordon,
-        name: 'Gordon Freeman',
-        knows: idAlyx
-      },
-      {
-        '@context': mySchemaUrl,
-        '@id': idAlyx,
-        name: 'Alyx Vence',
-        knows: idGordon
-      }
-    ]
-
     const contexts = {
-      '@base': myInstancesNs,
-      'foaf': 'http://xmlns.com/foaf/0.1/'
+      '@base': HALFLIFE_PERSON_NS,
+      foaf: FOAF_NS
     }
 
-    const { graph, id2obj } = await jsonld2obj(data, contexts)
-    // console.log(JSON.stringify(data, null, 2))
-    // console.log(graph)
+    const { graph, id2obj } = await jsonld2obj(HALFLIFE_JSONLD, contexts)
 
     expect(id2obj.Gordon).toEqual(id2obj.Alyx['foaf:knows'])
     expect(id2obj.Gordon['foaf:knows']['foaf:name']).toEqual('Alyx Vence')
-    expect(id2obj.Gordon['foaf:knows']['foaf:knows']['foaf:name']).toEqual('Gordon Freeman')
+    expect(id2obj.Gordon['foaf:knows']['foaf:knows']['foaf:name']).toEqual(
+      'Gordon Freeman'
+    )
     expect(() => JSON.stringify(graph)).toThrow('circular structure')
   })
 })
